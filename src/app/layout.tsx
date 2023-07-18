3f4791ce2cf9359e94e5024c1edbea69d475b492
Vinicius Ferreira <viniciuferreira32@gmail.com>
2023-07-18 13:45:51 -0300

    style: styles the base of the siderbar and header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
+import { Sidebar } from '@/components/Sidebar'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { Header } from '@/components/Header'
+import { HeroPattern } from '@/components/HeroPattern'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +19,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-zinc-900`}>{children}</body>
+      <body className={`${inter.className} bg-zinc-900`}>
+        <Sidebar />
+        <div className="ml-80 relative h-screen">
+          <Header />
+          <HeroPattern />
+          <div className="py-24 max-w-4xl px-8 mx-auto">{children}</div>
+        </div>
+      </body>
     </html>
   )
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,5 @@
+export function Header() {
+  return (
+    <header className="fixed top-0 left-80 right-0 px-8 py-3 border-b border-white/10"></header>
+  )
+}
